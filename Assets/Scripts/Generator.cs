e945f27c96547d17ce69e370ae66af2e434b7f84
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-09-07 17:12:07 +0900

    Fixed CSV Data

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -16,9 +16,9 @@ public class Generator : MonoBehaviour
         {
             count += DataManager.GetSkillLevel(0) * 0.1f * DataManager.Instance.Level;
             count += DataManager.GetSkillLevel(1) * 1 * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(2) * 3 * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(3) * 5 * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(4) * 10 * DataManager.Instance.Level;
+            count += DataManager.GetSkillLevel(2) * 15 * DataManager.Instance.Level;
+            count += DataManager.GetSkillLevel(3) * 100 * DataManager.Instance.Level;
+            count += DataManager.GetSkillLevel(4) * 500 * DataManager.Instance.Level;
             var integer = Mathf.Floor(count);
             DataManager.Instance.Count += (ulong)integer;
             count -= integer;
