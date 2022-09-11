b88fe0347057bdcfd90488ca7f77db6a48527511
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-09-12 03:19:35 +0900

    Fixed Bugs

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -9,6 +9,7 @@ public class Generator : MonoBehaviour
     [SerializeField] Button[] _button;
     float _time;
     float count;
+    BigInteger bigCount;
 
     void Update()
     {
@@ -16,17 +17,17 @@ public class Generator : MonoBehaviour
         if(_time >= 1f)
         {
             count += DataManager.GetSkillLevel(0) * 0.1f * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(1) * 1 * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(2) * 10 * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(3) * 100 * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(4) * 1000 * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(5) * 10000 * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(6) * 100000 * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(7) * 1000000 * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(8) * 10000000 * DataManager.Instance.Level;
-            count += DataManager.GetSkillLevel(9) * 100000000 * DataManager.Instance.Level;
+            bigCount += DataManager.GetSkillLevel(1) * 1 * DataManager.Instance.Level;
+            bigCount += DataManager.GetSkillLevel(2) * 10 * DataManager.Instance.Level;
+            bigCount += DataManager.GetSkillLevel(3) * 100 * DataManager.Instance.Level;
+            bigCount += DataManager.GetSkillLevel(4) * 1000 * DataManager.Instance.Level;
+            bigCount += DataManager.GetSkillLevel(5) * 10000 * DataManager.Instance.Level;
+            bigCount += DataManager.GetSkillLevel(6) * 100000 * DataManager.Instance.Level;
+            bigCount += DataManager.GetSkillLevel(7) * 1000000 * DataManager.Instance.Level;
+            bigCount += DataManager.GetSkillLevel(8) * 10000000 * DataManager.Instance.Level;
+            bigCount += DataManager.GetSkillLevel(9) * 100000000 * DataManager.Instance.Level;
             var integer = Mathf.Floor(count);
-            DataManager.Instance.Count += (BigInteger)integer;
+            DataManager.Instance.Count += ((BigInteger)integer + bigCount);
             count -= integer;
             _time = 0f;
         }
